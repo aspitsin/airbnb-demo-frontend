e3a97ec65ee9b07b5b4abbef41f4a6faedfc2893
aspitsin <pz94@bk.ru>
2018-01-03 09:47:21 +0300

    Adding header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
-import styled, { css } from "styled-components";
+import "./App.css";
 import Header from "./Header";
-import logo from "./Header/logo.svg";
+import Main from "./Main";
+import Footer from "./Footer";
+import styled from "styled-components";
 
+const Row = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div``;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </Header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Row>
+        <Container>
+          <Header />
+        </Container>
+      </Row>
     );
   }
 }
