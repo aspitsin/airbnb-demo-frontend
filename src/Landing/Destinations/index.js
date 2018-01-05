94f872734d8c078e77980ce0f16cf599b8dcae36
aspitsin <pz94@bk.ru>
2018-01-05 19:58:31 +0300

    Landing/Dest. fix (change .svg>.png; merge Card with Link;change City=styled.span>.p)

--- src/Landing/Destinations/index.js
+++ src/Landing/Destinations/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { List, Card, Link, Title, Image, SliderArrow } from "../styled";
-import img1 from "./Paris.svg";
-import img2 from "./Miami.svg";
-import img3 from "./Tokyo.svg";
-import img4 from "./CaperTown.svg";
-import img5 from "./Seul.svg";
-import img6 from "./LosAn.svg";
+import img1 from "./Paris.png";
+import img2 from "./Miami.png";
+import img3 from "./Tokyo.png";
+import img4 from "./Capetown.png";
+import img5 from "./Seoul.png";
+import img6 from "./LosAng.png";
 
 const Destinations = styled.section``;
 
-const City = styled.span`
+const City = styled.p`
   display: block;
   font-family: "CircularAirBold", "Helvetica Neue", Helvetica, Arial, sans-serif;
   font-size: 1rem;
@@ -28,57 +28,45 @@ export default () => {
       <div className="row">
         <List>
           <div className="col-lg-2">
-            <Card>
-              <Link href="/">
-                <Image src={img1} alt="" />
-                <City>Paris</City>
-              </Link>
-            </Card>
+            <Link href="/">
+              <Image src={img1} alt="" />
+              <City>Paris</City>
+            </Link>
           </div>
 
           <div className="col-lg-2">
-            <Card>
-              <Link href="/">
-                <Image src={img2} alt="" />
-                <City>Miami</City>
-              </Link>
-            </Card>
+            <Link href="/">
+              <Image src={img2} alt="" />
+              <City>Miami</City>
+            </Link>
           </div>
 
           <div className="col-lg-2">
-            <Card>
-              <Link href="/">
-                <Image src={img3} alt="" />
-                <City>Tokyo</City>
-              </Link>
-            </Card>
+            <Link href="/">
+              <Image src={img3} alt="" />
+              <City>Tokyo</City>
+            </Link>
           </div>
 
           <div className="col-lg-2">
-            <Card>
-              <Link href="/">
-                <Image src={img4} alt="" />
-                <City>Cape town</City>
-              </Link>
-            </Card>
+            <Link href="/">
+              <Image src={img4} alt="" />
+              <City>Cape town</City>
+            </Link>
           </div>
 
           <div className="col-lg-2">
-            <Card>
-              <Link href="/">
-                <Image src={img5} alt="" />
-                <City>Seoul</City>
-              </Link>
-            </Card>
+            <Link href="/">
+              <Image src={img5} alt="" />
+              <City>Seoul</City>
+            </Link>
           </div>
 
           <div className="col-lg-2">
-            <Card>
-              <Link href="/">
-                <Image src={img6} alt="" />
-                <City>Los Angeles</City>
-              </Link>
-            </Card>
+            <Link href="/">
+              <Image src={img6} alt="" />
+              <City>Los Angeles</City>
+            </Link>
           </div>
 
           <Arrow />
