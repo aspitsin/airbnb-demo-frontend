c140f222c6d6b6dabcdfedbe88a99553be8e24c2
aspitsin <pz94@bk.ru>
2018-01-08 23:54:55 +0300

    Experiences Add container

--- src/Landing/Experiences/index.js
+++ src/Landing/Experiences/index.js
@@ -8,60 +8,58 @@ import img3 from "./img3.png";
 import img4 from "./img4.png";
 import Card from "./Card";
 
-const Experiences = styled.section`
-  position: relative;
-`;
-
 const Arrow = SliderArrow.extend`
   top: 40%;
 `;
 
 export default () => {
   return (
-    <Experiences>
+    <section>
       <Title>
         Experiences
         <SeeAll href="/">See all</SeeAll>
       </Title>
-      <div className="row">
-        <Slider>
-          <div className="col-xs-6 col-md-4 col-lg-3">
-            <Card
-              title="Forest Therapy"
-              image={img1}
-              price="29$"
-              review="44 reviews"
-            />
-          </div>
+      <div className="container">
+        <div className="row">
+          <Slider>
+            <div className="col-xs-6 col-md-4 col-lg-3">
+              <Card
+                title="Forest Therapy"
+                image={img1}
+                price="29$"
+                review="44 reviews"
+              />
+            </div>
 
-          <div className="col-xs-6 col-md-4 col-lg-3">
-            <Card
-              title="Whale watching"
-              image={img2}
-              price="69$"
-              review="46 reviews"
-            />
-          </div>
-          <div className="col-xs-6 col-md-4 col-lg-3">
-            <Card
-              title="Table Mountain Summit, Cable Car Downing"
-              image={img3}
-              price="69$"
-              review="44 reviews"
-            />
-          </div>
+            <div className="col-xs-6 col-md-4 col-lg-3">
+              <Card
+                title="Whale watching"
+                image={img2}
+                price="69$"
+                review="46 reviews"
+              />
+            </div>
+            <div className="col-xs-6 col-md-4 col-lg-3">
+              <Card
+                title="Table Mountain Summit, Cable Car Downing"
+                image={img3}
+                price="69$"
+                review="44 reviews"
+              />
+            </div>
 
-          <div className="col-xs-6 col-md-4 col-lg-3">
-            <Card
-              title="Salsa night"
-              image={img4}
-              price="50$"
-              review="44 reviews"
-            />
-          </div>
-          <Arrow />
-        </Slider>
+            <div className="col-xs-6 col-md-4 col-lg-3">
+              <Card
+                title="Salsa night"
+                image={img4}
+                price="50$"
+                review="44 reviews"
+              />
+            </div>
+            <Arrow />
+          </Slider>
+        </div>
       </div>
-    </Experiences>
+    </section>
   );
 };
