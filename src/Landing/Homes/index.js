ae91330bc5413c34dacc6eb5d6320f30c8ac0c53
aspitsin <pz94@bk.ru>
2018-01-09 00:03:27 +0300

    Homes Add container

--- src/Landing/Homes/index.js
+++ src/Landing/Homes/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { Title } from "../styled";
-import { Card, Link, SeeAll, Image, SliderArrow, Slider } from "../styled";
+import { Card, Link, SeeAll, Img, SliderArrow, Slider } from "../styled";
 import img1 from "./relax.png";
 import img2 from "./privatebedr.png";
 import img3 from "./TropicalHouse.png";
-import StarWrapper from "../Stars/index";
-
-const Homes = styled.section`
-  position: relative;
-`;
+import Stars from "../Stars/index";
 
 const CardTitle = styled.h2`
   font-family: "CircularAirBold", "Helvetica Neue", Helvetica, Arial, sans-serif;
@@ -46,52 +42,54 @@ const Arrow = SliderArrow.extend`
 
 export default () => {
   return (
-    <Homes>
+    <section>
       <Title>
         Homes
         <SeeAll href="/">See all</SeeAll>
       </Title>
-      <div className="row">
-        <Slider>
-          <div className="col-xs-8 col-md-5 col-lg-4">
-            <Card>
-              <Link href="/">
-                <Image src={img1} alt="" />
-                <CardTitle>$82 La Salentina, see, nature & relax</CardTitle>
-              </Link>
-              <Description>Entire house · 9 beds</Description>
-              <StarWrapper />
-              <Superhost>97 · Superhost</Superhost>
-            </Card>
-          </div>
-          <div className="col-xs-8 col-md-5 col-lg-4">
-            <Card>
-              <Link href="/">
-                <Image src={img2} alt="" />
-                <CardTitle>
-                  $82 Your private 3 bedr. riad and exclusi...
-                </CardTitle>
-              </Link>
-              <Description>Entire house · 5 beds</Description>
-              <StarWrapper />
-              <Superhost>161 · Superhost</Superhost>
-            </Card>
-          </div>
-          <div className="col-xs-8 col-md-5 col-lg-4">
-            <Card>
-              <Link href="/">
-                <Image src={img3} alt="" />
-                <CardTitle>$200 Dreamy Tropical Tree House</CardTitle>
-              </Link>
-              <Description>Entire treehouse · 1 bed</Description>
-              <StarWrapper />
-              <Superhost>364 · Superhost</Superhost>
-            </Card>
-          </div>
+      <div className="container">
+        <div className="row">
+          <Slider>
+            <div className="col-xs-8 col-md-5 col-lg-4">
+              <Card>
+                <Link href="/">
+                  <Img src={img1} alt="" />
+                  <CardTitle>$82 La Salentina, see, nature & relax</CardTitle>
+                </Link>
+                <Description>Entire house · 9 beds</Description>
+                <Stars />
+                <Superhost>97 · Superhost</Superhost>
+              </Card>
+            </div>
+            <div className="col-xs-8 col-md-5 col-lg-4">
+              <Card>
+                <Link href="/">
+                  <Img src={img2} alt="" />
+                  <CardTitle>
+                    $82 Your private 3 bedr. riad and exclusi...
+                  </CardTitle>
+                </Link>
+                <Description>Entire house · 5 beds</Description>
+                <Stars />
+                <Superhost>161 · Superhost</Superhost>
+              </Card>
+            </div>
+            <div className="col-xs-8 col-md-5 col-lg-4">
+              <Card>
+                <Link href="/">
+                  <Img src={img3} alt="" />
+                  <CardTitle>$200 Dreamy Tropical Tree House</CardTitle>
+                </Link>
+                <Description>Entire treehouse · 1 bed</Description>
+                <Stars />
+                <Superhost>364 · Superhost</Superhost>
+              </Card>
+            </div>
 
-          <Arrow />
-        </Slider>
+            <Arrow />
+          </Slider>
+        </div>
       </div>
-    </Homes>
+    </section>
   );
 };
