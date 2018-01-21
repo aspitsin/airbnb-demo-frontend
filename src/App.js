b97d2e33575b8842ed999d6f15f88aa8d3a2dcc6
aspitsin <pz94@bk.ru>
2018-01-21 21:50:30 +0300

    Fix link on Main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Main from "./Main.js";
+import Main from "./Main";
 import HomesPage from "./HomesPage";
 import Header from "./Header";
 
